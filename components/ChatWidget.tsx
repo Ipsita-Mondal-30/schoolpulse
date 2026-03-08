90c54eba63aff817e2bf4bac333ca9cca5758ef8
Sreedhara GS <sreedhargs89@gmail.com>
2026-03-08 09:59:42 +0900

    style(chat): change notification dot to green to indicate online status

--- components/ChatWidget.tsx
+++ components/ChatWidget.tsx
@@ -227,8 +227,8 @@ export default function ChatWidget() {
             >
                 <MessageCircle size={28} className="drop-shadow-sm" />
                 <span className="absolute -top-1 -right-1 flex h-4 w-4">
-                    <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-white opacity-40"></span>
-                    <span className="relative inline-flex rounded-full h-4 w-4 bg-orange-200 border border-orange-500"></span>
+                    <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-60"></span>
+                    <span className="relative inline-flex rounded-full h-4 w-4 bg-green-500 border-2 border-white shadow-sm"></span>
                 </span>
             </button>
 
